27c0dce45f37dddafda582bdae9fcca5d68f1a8d
unknown <felipe.jr.fonseca@gmail.com>
2021-08-23 14:30:10 -0300

    main

---
+++ etapas.js
@@ -0,0 +1,24 @@
+let etapas = [
+    {
+        titulo: 'VEREADOR',
+        numeros: 5,
+        candidato: [
+            {
+                numero: '11111',
+                nome: 'Vin Diesel do Py',
+                partido: 'PYP',
+                foto:[
+                    {url:'img/Vin Diesel.jpg', legenda: 'Vereador'}
+                ]
+            },
+            {
+                numero: '55555',
+                nome: 'Zoio',
+                partido: 'BRZ',
+                foto:[
+                    {url:'img/Zoio.jpg', legenda: 'Vereador'}
+                ]
+            },
+        ]
+    }
+]
--- scripts.js
+++ scripts.js
@@ -1,41 +1,77 @@
-let SeuVotoPara = document.querySelector('.d-1-1 span');
-let cargo = document.querySelector('d-1-2 span');
-let descricao = document.querySelector('d-1-4');
-let aviso = document.querySelector('d-2');
-let lateral = document.querySelector('d-1-right');
-let numeros = document.querySelector('d-1-3');
+let seuVotoPara = document.querySelector('.d-1-1 span');
+let cargo = document.querySelector('.d-1-2 span');
+let descricao = document.querySelector('.d-1-4');
+let aviso = document.querySelector('.d-2');
+let lateral = document.querySelector('.d-1-right');
+let numeros = document.querySelector('.d-1-3');
 
 let etapaAtual = 0;
-//let etapas = '';
+let numero = '';
+
 function comecarEtapa(){
-    //let etapa = etapas[etapaAtual];
-    lateral.innerHTML = '';
+    let etapa = etapas[etapaAtual];
     let numeroHtml = '';
 
-    for(let i=0; i<etapa.numeroHtml; i++){
-        numeroHtml += '<div class="numero"></div>';
+    for(let i=0; i<etapa.numeros; i++){
+        if(i === 0){
+            numeroHtml += '<div class="numero pisca"></div>'
+        }
+        else{
+            numeroHtml += '<div class="numero"></div>';
+        }    
     }
 
-    SeuVotoPara.style.display = 'none';
+    seuVotoPara.style.display = 'none';
     cargo.innerHTML = etapa.titulo;
     descricao.innerHTML = '';
-    aviso.style.disblay = 'none';
+    aviso.style.display = 'none';
     lateral.innerHTML = '';
     numeros.innerHTML = numeroHtml;
-    alert("Funcionando 2");
+}
+
+function atualizarInterface(){
+    let etapa = etapas[etapaAtual];
+    let candidato = etapa.candidato.filter((item)=>{
+        if(item.numero === numero){
+            return true;
+        }
+        else{
+            return false;
+        }
+
+    });
+    if(canditado.length > 0){
+        candidato = candidato[0];
+        seuVotoPara.style.display = 'block';
+        descricao.innerHTML = `Nome: ${candidato.nome}<br/>Partido: ${candidato.partido}`;
+        aviso.style.display = 'block';
+    }
+
 }
 
 function clicou(n){
-    alert("Clicou em "+n);
+    let elNumero = document.querySelector('.numero.pisca');
+    if( elNumero !== null){
+        elNumero.innerHTML = n;
+        numero = `${numero}${n}`;
+
+        elNumero.classList.remove('pisca');
+        if(elNumero.nextElementSibling !== null){
+            elNumero.nextElementSibling.classList.add('pisca');
+        }
+        else{
+            atualizarInterface();
+        }
+    }
 }
 function branco(){
-    alert("Clicou em BRANCO");
+    alert("clicou em BRANCO!");
 }
 function corrige(){
-    alert("Clicou em CORRIGE");
+    alert("clicou em CORRIGE!");
 }
 function confirma(){
-    alert("Clicou em CONFIRMA");
+    alert("clicou em CONFIRMA!");
 }
 
 comecarEtapa();
